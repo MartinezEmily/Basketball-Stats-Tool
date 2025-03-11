0b8f03021f2a6a87f8d47412c662bd0b7fde1d22
MartinezEmily <Emily.Martinez039@gmail.com>
2025-03-10 22:30:39 -0400

    Made code edits

--- basketball-team-stats_v1/app.py
+++ basketball-team-stats_v1/app.py
@@ -23,24 +23,27 @@ def clean_data(data):   # Clean guardian, experience, and height data of each pl
     return cleaned
 
 
-def balance_teams(teams, players):    #  Distribute players evenly across teams, balancing total number and experience
+def balance_teams(teams, players):    # Distribute players evenly across teams, balancing total number and experience
     experienced = []
     inexperienced = []
 
-    for player in players:
+    for player in players:  # Create two categories: experienced vs inexperienced
         if player['experience'] == 'YES':
             experienced.append(player)
         else:
             inexperienced.append(player)
 
     # Experience level distribution
-    experienced_players_per_team = len(experienced) // len(teams)
-    inexperienced_players_per_team = len(inexperienced) // len(teams)
+    experienced_players_per_team = len(experienced) // len(teams)   # 3 experienced players per team
+    inexperienced_players_per_team = len(inexperienced) // len(teams)   # 3 inexperienced players per team
 
-
+    # Distribute players evenly across teams
+    
 
 
 if __name__ == "__main__":              # Running app.py file directly
 
-    cleaned_players= clean_data(PLAYERS)  # calling clean_data function
+    cleaned_players = clean_data(PLAYERS)  # calling clean_data function
     print(cleaned_players)
+
+    balance_teams(TEAMS, PLAYERS)
