056e883c30909dd0423848160e3e39815a257b9e
MartinezEmily <Emily.Martinez039@gmail.com>
2025-03-10 21:56:32 -0400

    Distributed players by experience

--- basketball-team-stats_v1/app.py
+++ basketball-team-stats_v1/app.py
@@ -1,13 +1,14 @@
 from constants import PLAYERS, TEAMS    # Importing constants file
 
-def clean_data(data):   # Fix guardian, experience and height of each player data
+
+def clean_data(data):   # Clean guardian, experience, and height data of each player
     cleaned = []
     
     for player in data:
         fixed = {}
         # Keep the name of player
         fixed["name"] = player["name"] 
-        print(player["email"])
+        print(player["name"])
         # Split guardian into a list
         fixed["guardian"] = player["guardians"].split(" and ")
         # Convert experience to boolean
@@ -22,7 +23,24 @@ def clean_data(data):   # Fix guardian, experience and height of each player dat
     return cleaned
 
 
+def balance_teams(teams, players):    #  Distribute players evenly across teams, balancing total number and experience
+    experienced = []
+    inexperienced = []
+
+    for player in players:
+        if player['experience'] == 'YES':
+            experienced.append(player)
+        else:
+            inexperienced.append(player)
+
+    # Experience level distribution
+    experienced_players_per_team = len(experienced) // len(teams)
+    inexperienced_players_per_team = len(inexperienced) // len(teams)
+
+
+
+
 if __name__ == "__main__":              # Running app.py file directly
 
-    cleaned_players = clean_data(PLAYERS)  # calling clean_data function
+    cleaned_players= clean_data(PLAYERS)  # calling clean_data function
     print(cleaned_players)
